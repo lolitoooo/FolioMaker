6a426e7904b01c360a67d7c2408ddb0199de7f6b
Ubuntu <lpena@myges.fr>
2023-11-06 01:04:01 +0000

    Add connection Mysql

---
+++ script/script.sql
@@ -0,0 +1,19 @@
+-- Supprimer la table user si elle existe
+DROP TABLE IF EXISTS user;
+
+-- Créer la table user avec les colonnes id, name, lastname, mail, et password
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    mail VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Insérer 5 enregistrements avec des noms, prénoms, mails et mots de passe fictifs
+INSERT INTO users (name, lastname, mail, password) VALUES
+    ('John', 'Doe', 'john.doe@example.com', 'motdepasse1'),
+    ('Jane', 'Smith', 'jane.smith@example.com', 'motdepasse2'),
+    ('Alice', 'Johnson', 'alice.johnson@example.com', 'motdepasse3'),
+    ('Bob', 'Williams', 'bob.williams@example.com', 'motdepasse4'),
+    ('Eve', 'Brown', 'eve.brown@example.com', 'motdepasse5');
--- www/Views/Templates/back.tpl.php
+++ www/Views/Templates/back.tpl.php
@@ -1,3 +1,6 @@
+<?php 
+require_once __DIR__ . '/../../functions/database/connect.php';
+?>
 <!DOCTYPE html>
 <html lang="fr">
     <head>
