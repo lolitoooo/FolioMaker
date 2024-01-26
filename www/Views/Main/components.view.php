7d4241a94619025fa3dbd1567ffd777054722115
Maheanuu Allain <maheanuu.allain@gmail.com>
2024-01-26 19:10:35 +0100

    feat: add style guide page, button, image, input, tootip components

--- www/Views/Main/components.view.php
+++ www/Views/Main/components.view.php
@@ -1,9 +1,9 @@
-<div>
-    <h1 style="font-family: 'Roboto';">Nos composants</h1>
-    
-    
-   <div>
+
+<h1 style="font-family: 'Roboto';">Nos composants</h1>
+<div style="display: flex; flex-direction: column; gap: 2rem;">
+   <div style="display: flex; flex-direction:column; gap: 1rem;">
     <h2>Buttons</h2>
+    <h4>Normal</h4>
         <div style="display: flex; gap: 1rem">
             <button class="button button-primary">Button</button>
             <button class="button button-secondary">Button</button>
@@ -21,6 +21,28 @@
                 Button
             </button>
             <button class="button button-secondary button-icon">
+                Button
+                <img style="width: 1rem; height: 1rem; background: none;" src="../../src/assets/icons/right-arrow.svg" alt="">
+            </button>
+        </div>
+    <h4>Small</h4>
+        <div style="display: flex; gap: 1rem">
+            <button class="button button-primary button-small">Button</button>
+            <button class="button button-secondary button-small">Button</button>
+            <button class="button button-borderless button-small">Button</button>
+            <button class="button button-icon button-small">
+                <img style="width: 1rem; height: 1rem; background: none; filter: invert(100%);" src="../../src/assets/icons/left-arrow.svg" alt="">
+                Button
+            </button>
+            <button class="button button-icon button-small">
+                Button
+                <img style="width: 1rem; height: 1rem; background: none; filter: invert(100%);" src="../../src/assets/icons/right-arrow.svg" alt="">
+            </button>
+            <button class="button button-secondary button-icon button-small">
+                <img style="width: 1rem; height: 1rem; background: none;" src="../../src/assets/icons/left-arrow.svg" alt="">
+                Button
+            </button>
+            <button class="button button-secondary button-icon button-small">
                 Button
                 <img style="width: 1rem; height: 1rem; background: none;" src="../../src/assets/icons/right-arrow.svg" alt="">
             </button>
@@ -50,6 +72,29 @@
             </div>
             <div style="width: 8rem; height: 8rem;" class="shadow-xxlarge">
                 <p>xxlarge</p>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <h2>Inputs</h2>
+        <div style="display: flex; flex-direction: column; gap: 1rem">
+            <input type="text" class="input">
+            <textarea class="input" name="" id="" cols="30" rows="10"></textarea>
+            <div class="input-action-square">
+                <input type="radio" id="radio1" name="radio">
+                <label for="radio1"></label>
+            </div>
+            <input class="input-action" type="radio" name="" id="">
+        </div>
+    </div>
+
+    <div>
+        <h2>Tooltips</h2>
+        <div style="display: flex; flex-direction: column; gap: 1rem">
+            <div class="tooltip">
+                <p>Hello World it's tooltip</p>
+                <img src="www/src/assets/icons/arrow-bottom.svg" alt="">
             </div>
         </div>
     </div>
--- www/Views/Templates/front.tpl.php
+++ www/Views/Templates/front.tpl.php
@@ -4,11 +4,10 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../../dist/main.css">
         <link rel="icon" type="image/svg" href="../../src/assets/logo/LogoFolioMaker.svg" />
-        <title>Front</title>
+        <title>Front Template</title>
     </head>
     <body>
         <img src="../../src/assets/logo/LogoAllFolioMaker.svg" alt="Logo" width="300px">
-        <h1>Template du front</h1>
         <?php include $this->view;?>
     </body>
 </html>
