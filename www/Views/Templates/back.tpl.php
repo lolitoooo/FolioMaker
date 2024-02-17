609037beab4dfe21ef5c8f7d86ff21341c07476b
Maheanuu Allain <maheanuu.allain@gmail.com>
2024-02-17 20:32:57 +0100

    refactor: changement d'icons et ajout de fontawesome

--- www/Views/Templates/back.tpl.php
+++ www/Views/Templates/back.tpl.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../../dist/css/style.css">
         <link rel="icon" type="image/svg" href="../../src/assets/logo/LogoFolioMaker.svg" />
+        <script src="https://kit.fontawesome.com/18a6431c94.js" crossorigin="anonymous"></script>
         <title>Back Template</title>
     </head>
     <body>
