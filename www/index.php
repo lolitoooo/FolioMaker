e0424e041d11b6c18b58d63b3247c398f12203cc
lolitoooo <loanpena77@gmail.com>
2024-02-19 10:46:11 +0100

    Fix routing index + conflicts

--- www/index.php
+++ www/index.php
@@ -2,59 +2,59 @@
 
 namespace App;
 
-session_start();
-
 spl_autoload_register("App\myAutoloader");
 function myAutoloader($class)
 {
-    $class = str_replace("App\\", "", $class);
-    $class = str_replace("\\", "/", $class) . ".php";
-    if (file_exists($class)) {
-        include $class;
+    $file = str_replace("App\\", "", $class);
+    $file = str_replace("\\", "/", $file);
+    $file .=".php";
+    if(file_exists($file)){
+        include $file;
     }
 }
 
 $uri = strtolower($_SERVER["REQUEST_URI"]);
 $uri = strtok($uri, "?");
-if (strlen($uri) > 1) $uri = rtrim($uri, "/");
+if(strlen($uri )>1) $uri = rtrim($uri, "/");
 
 $fileRoute = "routes.yaml";
-if (!file_exists($fileRoute)) {
-    die("Le fichier de routing n'existe pas.");
+if(file_exists($fileRoute)) {
+    $listOfRoutes = yaml_parse_file($fileRoute);
+}else{
+    die("Le fichier de routing n'existe pas");
 }
-$listOfRoutes = yaml_parse_file($fileRoute);
 
-$foundRoute = false;
-foreach ($listOfRoutes as $route => $info) {
-    $routePattern = preg_replace('#\{([a-z]+)\}#', '([^/]+)', $route);
-    if (preg_match('#^' . $routePattern . '$#', $uri, $matches)) {
-        array_shift($matches); 
-        $foundRoute = true;
-        $controller = $info["controller"];
-        $action = $info["action"];
+if(!empty($listOfRoutes[$uri])){
+    if(!empty($listOfRoutes[$uri]["controller"])){
+        if(!empty($listOfRoutes[$uri]["action"])){
 
-        $controllerFile = "Controllers/" . $controller . ".php";
-        if (file_exists($controllerFile)) {
-            include $controllerFile;
-            $controllerClass = "App\\Controllers\\" . $controller;
-            if (class_exists($controllerClass)) {
-                $object = new $controllerClass();
-                if (method_exists($object, $action)) {
-                    call_user_func_array([$object, $action], $matches);
-                } else {
-                    die("L'action " . $action . " n'existe pas.");
+            $controller = $listOfRoutes[$uri]["controller"];
+            $action = $listOfRoutes[$uri]["action"];
+
+            if(file_exists("Controllers/".$controller.".php")){
+                include "Controllers/".$controller.".php";
+                $controller = "App\\Controllers\\".$controller;
+                if(class_exists($controller)){
+                    $object = new $controller();
+                if(method_exists($object, $action)){
+                    $object->$action();
+                }else{
+                    die("L'action' ".$action. " n'existe pas");
                 }
-            } else {
-                die("La classe " . $controllerClass . " n'existe pas.");
+                }else{
+                    die("Le class controller ".$controller. " n'existe pas");
+                }
+            }else{
+                die("Le fichier controller ".$controller. " n'existe pas");
             }
-        } else {
-            die("Le fichier " . $controllerFile . " n'existe pas.");
-        }
-        break;
-    }
-}
 
-if (!$foundRoute) {
+        }else{
+            die("La route ".$uri." ne possède pas d'action dans le ficher ".$fileRoute);
+        }
+    }else{
+        die("La route ".$uri." ne possède pas de controller dans le ficher ".$fileRoute);
+    }
+}else{
     include "Controllers/Error.php";
     $object = new Controllers\Error();
     $object->page404();
