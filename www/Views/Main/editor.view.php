1c98471c016929ed132adbc282228987c0ff896b
Maheanuu Allain <maheanuu.allain@gmail.com>
2024-02-19 14:30:00 +0100

    refactor: modification des dependances et du style de l'editor

--- www/Views/Main/editor.view.php
+++ www/Views/Main/editor.view.php
@@ -1,2 +1,15 @@
+<style>
+    #gjs {border: none;}
+    .gjs-one-bg {background-color: #12100E;}
+    .gjs-two-color {color: #F4F4F4;}
+    .gjs-three-bg {
+        background-color: #ec5896;
+        color: white;
+    }
+    .gjs-four-color,
+    .gjs-four-color-h:hover {
+    color: #256EFF;
+    }
+</style>
 <div id="gjs">
 </div>
