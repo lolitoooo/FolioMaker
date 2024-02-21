ca81449d9e0ab217a8f439398a6b7e6b266ed56e
lolitoooo <loanpena77@gmail.com>
2024-02-21 23:30:40 +0100

    Fix css installer 2

--- www/Views/Modals/form.php
+++ www/Views/Modals/form.php
@@ -1,21 +1,44 @@
+<?php if(!empty($errors)):?>
+<div style="background-color: red;">
+    <?php echo implode("<br>", $errors); ?>
+</div>
+<?php endif; ?>
+
 <div class="form_container">
-    <h3><?php echo $title; ?></h3>
-    <?php if (!empty($errors)) : ?>
-        <div class="error-message">
-            <?php echo implode("<br>", $errors); ?>
-        </div>
+<h3><?= $title; ?></h3>
+
+<form
+    action="<?= $config["config"]["action"] ?? "" ?>"
+    method="<?= $config["config"]["method"] ?? "POST" ?>"
+    id="<?= $config["config"]["id"] ?? "" ?>"
+    class="<?= $config["config"]["class"] ?? "" ?>"
+>
+    <?php 
+    $hasIndividualSubmitButtons = false; 
+
+    foreach ($config["inputs"] as $name => $input): ?>
+        <div>
+            <input
+                name="<?= $name ?>"
+                type="<?= $input["type"] ?? "text" ?>"
+                class="<?= $input["class"] ?? "" ?>"
+                id="<?= $input["id"] ?? "" ?>"
+                placeholder="<?= $input["placeholder"] ?? "" ?>"
+                value="<?= htmlspecialchars($input["value"] ?? '') ?>"
+                <?= $input["required"] ? "required" : "" ?>
+            >
+
+            <?php if(isset($input["submitLabel"])): 
+                $hasIndividualSubmitButtons = true; 
+            ?>
+                <!-- Bouton de soumission spécifique pour ce champ -->
+                <button class="button" type="submit" name="action" value="update<?= ucfirst($name) ?>"><?= $input["submitLabel"] ?></button>
+            <?php endif; ?>
+        </div><br>
+    <?php endforeach; 
+
+    if (!$hasIndividualSubmitButtons): ?>
+        <input class="button" type="submit" value="<?= $config["config"]["submit"] ?? "Envoyer" ?>">
     <?php endif; ?>
-
-    <form action="<?= $config["config"]["action"] ?? "" ?>" method="<?= $config["config"]["method"] ?? "POST" ?>" id="<?= $config["config"]["id"] ?? "" ?>" class="<?= $config["config"]["class"] ?? "" ?>">
-
-        <?php foreach ($config["inputs"] as $name => $input) : ?>
-            <div class="input-container"> <!-- Ajout du conteneur pour chaque input -->
-                <label for="<?= $input["id"] ?? "" ?>"><?= $input["placeholder"] ?? "" ?></label>
-                <input name="<?= $name ?>" type="<?= $input["type"] ?? "text" ?>" class="form-input <?= $input["class"] ?? "" ?>" id="<?= $input["id"] ?? "" ?>" placeholder="<?= $input["placeholder"] ?? "" ?>" value="<?= htmlspecialchars($input["value"] ?? '') ?>" <?= $input["required"] ? "required" : "" ?>>
-            </div>
-        <?php endforeach; ?>
-        <button type="submit" class="form-input button"><?= $config["config"]["submit"] ?? "Submit" ?></button>
-
-
-    </form>
+</form>
 </div>
